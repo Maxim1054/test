8254d7664a19021678ee3e92c731dbe99469eb77
peterson763 <melnikov.maksim.40@bk.ru>
2021-05-22 13:04:54 +0300

    test
    test

---
+++ тест 1.py
@@ -0,0 +1,6 @@
+def test_per():
+    assert 4 * 5 == 20, "Should be 20"
+
+if __name__ == "__main__":
+    test_per()
+    print("Everything passed")
---
+++ тест 3.py
@@ -0,0 +1,6 @@
+def test_fun():
+    assert 3*5*5*5*5-6*5*5-7 == 1718, "Should be 1718"
+
+if __name__ == "__main__":
+    test_fun()
+    print("Everything passed")
